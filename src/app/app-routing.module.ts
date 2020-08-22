f3dd42ad444e071b786010bf72608d97ef0a9b0b
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-22 08:31:41 -0500

    [ADD] routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,24 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProducFormComponent } from './components/produc-form/produc-form.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ProductListComponent
+  },
+  {
+    path: 'product',
+    component: ProductListComponent
+  },
+  {
+    path: 'product/create',
+    component: ProducFormComponent
+  },
+  {
+    path: 'product/edit/:id',
+    component: ProducFormComponent
   }
 ];
 
