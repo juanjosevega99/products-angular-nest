68c8530157327601886d2894239d40fa1034b80f
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-22 13:30:07 -0500

    [FEATURE] services crud

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -1,9 +1,39 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+import { Product } from "../interfaces/Products";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ProductService {
+  BASE_URL: string = "http://localhost:3000";
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getProducts() {
+    return this.http.get<Product[]>(`${this.BASE_URL}/product`);
+  }
+
+  getProduct(id: string): Observable<Product> {
+    return this.http.get<Product>(`${this.BASE_URL}/product/${id}`);
+  }
+
+  createProduct(product: Product): Observable<Product> {
+    return this.http.post<Product>(`${this.BASE_URL}/product/create`, product);
+  }
+
+  deleteProduct(id: string): Observable<Product> {
+    return this.http.delete<Product>(
+      `${this.BASE_URL}/product/delete?productID=${id}`
+    );
+  }
+
+  updateProduct(id: string, product: Product): Observable<Product> {
+    return this.http.put<Product>(
+      `${this.BASE_URL}/product/update?productID=${id}`,
+      product
+    );
+  }
 }
